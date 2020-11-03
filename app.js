a149bb2c2df3975140a704dcedba87dd4ea45b86
Chru <Tesfaye.Chru1@T-Mobile.com>
2020-11-03 10:36:05 -0600

    variable port

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const nodemon = require('nodemon');
 const app = express();
 
-app.listen(7000, () => {
-    console.log('ADD TEN APP');
-});
+const port = process.env.port || 7000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the home page');
 })
+
+app.listen(port, () => {
+    console.log('ADD TEN APP');
+});
+
